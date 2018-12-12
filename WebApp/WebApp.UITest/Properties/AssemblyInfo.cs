c4b22e21a7d28903d73d7f4fc421da59e6c0807a
Adam Barlow <adbarlow@microsoft.com>
2018-12-12 12:08:38 -0800

    Added Selenium Tests

---
+++ WebApp/WebApp.UITest/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("WebApp.UITest")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("WebApp.UITest")]
+[assembly: AssemblyCopyright("Copyright ©  2018")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("9bb47b39-ef1e-4389-a936-911e99ef03dd")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ WebApp/WebApp.UITest/UnitTest1.cs
@@ -0,0 +1,108 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using OpenQA.Selenium.Remote;
+using OpenQA.Selenium.Chrome;
+
+namespace WebApp.UITest
+{
+    [TestClass]
+    public class UITests
+    {
+        private static RemoteWebDriver _webDriver = null;
+        private static string _webAppBaseURL;
+
+        [ClassInitialize()]
+        public static void ClassInit(TestContext context)
+        {
+            // The hosted agent env variable for IE is IEWebDriver, Firefox is GeckoWebDriver
+            // If you switch the driver you'll need the corresponding nuget package.
+            _webDriver = new ChromeDriver(Environment.GetEnvironmentVariable("ChromeWebDriver"));
+
+            // Allow for web app compilation and startup post deployment 
+            _webDriver.Manage().Timeouts().ImplicitWait = TimeSpan.FromSeconds(15);
+
+            _webAppBaseURL = "https://metliftdelete.scm..azurewebsites.net/";
+        }
+
+        [ClassCleanup()]
+        public static void Cleanup()
+        {
+            if (_webDriver != null)
+            {
+                _webDriver.Quit();
+            }
+        }
+
+        [TestMethod]
+        [TestCategory("Selenium")]
+        public void HomePageFoundTest()
+        {
+            _webDriver.Url = _webAppBaseURL;
+
+            string actualPageTitle = _webDriver.Title;
+            string expectedPageTitle = "Home Page - My ASP.NET Application";
+
+            Assert.AreEqual(expectedPageTitle, actualPageTitle);
+        }
+
+        [TestMethod]
+        [TestCategory("Selenium")]
+        public void AboutPageFoundTest()
+        {
+            _webDriver.Url = _webAppBaseURL + "/Home/About";
+
+            string actualPageTitle = _webDriver.Title;
+            string expectedPageTitle = "About - My ASP.NET Application";
+
+            Assert.AreEqual(expectedPageTitle, actualPageTitle);
+        }
+
+        [TestMethod]
+        [TestCategory("Selenium")]
+        public void ContactPageFoundTest()
+        {
+            _webDriver.Url = _webAppBaseURL + "/Home/Contact";
+
+            string actualPageTitle = _webDriver.Title;
+            string expectedPageTitle = "Contact - My ASP.NET Application";
+
+            Assert.AreEqual(expectedPageTitle, actualPageTitle);
+        }
+
+        [TestMethod]
+        [TestCategory("Selenium")]
+        public void SupportEmailAddressChromeTest()
+        {
+            string supportEmailAddress = "Support@example.com";
+
+            _webDriver.Url = _webAppBaseURL + "/Home/Contact";
+            RemoteWebElement supportEmailElement = (RemoteWebElement)_webDriver.FindElementByLinkText(supportEmailAddress);
+
+            Assert.AreEqual(supportEmailAddress, supportEmailElement.Text);
+        }
+
+        [TestMethod]
+        [TestCategory("Selenium")]
+        public void MarketingEmailAddressTest()
+        {
+            string marketingEmailAddress = "Marketing@example.com";
+
+            _webDriver.Url = _webAppBaseURL + "/Home/Contact";
+            RemoteWebElement marketingEmailElement = (RemoteWebElement)_webDriver.FindElementByLinkText(marketingEmailAddress);
+
+            Assert.AreEqual(marketingEmailAddress, marketingEmailElement.Text);
+        }
+
+        [TestMethod]
+        [TestCategory("Selenium")]
+        public void IndexTitleTest()
+        {
+            string expectedTitle = "MY ASP.NET";
+
+            _webDriver.Url = _webAppBaseURL + "/Home/Index";
+            RemoteWebElement titleElement = (RemoteWebElement)_webDriver.FindElementByTagName("H1");
+
+            Assert.AreEqual(expectedTitle, titleElement.Text);
+        }
+    }
+}
